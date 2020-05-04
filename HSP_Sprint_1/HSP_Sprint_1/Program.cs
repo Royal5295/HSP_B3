d051b1eddb4800f744b387e82d655f0b65bf4a30
Royal5295 <62688901+Royal5295@users.noreply.github.com>
2020-05-04 23:13:41 +0200

    Update Program.cs

--- HSP_Sprint_1/HSP_Sprint_1/Program.cs
+++ HSP_Sprint_1/HSP_Sprint_1/Program.cs
@@ -10,63 +10,78 @@ namespace HSP_Sprint_1
     {
         static void Main(string[] args)
         {
-            try
-            {
-
-
-                // Anweisungen mit Variablen
-                Console.Write("Enter the rectangle's height:");
-                double height = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Enter the rectangle's width:");
-                double width = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Enter the rectangle's length:");
-                double length = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Enter the rectangle's Mass:");
-                double Mass = Convert.ToDouble(Console.ReadLine());
-
-
-                // Calculations
 
 
 
-                double Volume = (height * width * length);
+           
 
-                double Area = (width * height);
-
-                double Density = (Mass / Volume);
-
-                double IXX = ((height * width * width * width) / 12);
-
-                double IYY = ((height * height * height * width) / 12);
-
-                double Perimeter = (2 * (width + height));
-            }
-
-            catch
-            {
-                Console.WriteLine("Error");
-
-            }
+            
+            
+                try
+                {
 
 
+                    // Anweisungen mit Variablen
+                    Console.Write("Enter the rectangle's height:");
+                    double height = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter the rectangle's width:");
+                    double width = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter the rectangle's length:");
+                    double length = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter the rectangle's Mass:");
+                    double Mass = Convert.ToDouble(Console.ReadLine());
+
+
+                    // Calculations
+
+
+
+                    double Volume = (height * width * length);
+
+                    double Area = (width * height);
+
+                    double Density = (Mass / Volume);
+
+                    double IXX = ((height * width * width * width) / 12);
+
+                    double IYY = ((height * height * height * width) / 12);
+
+                    double Perimeter = (2 * (width + height));
+
+                    // Ausgaben
+
+                    Console.WriteLine("Perimeter of rectangle = {0}", Perimeter);
+                    Console.WriteLine("Area of rectangle = {0}", Area);
+                    Console.WriteLine("Volume of rectangle ={0}", Volume);
+                    Console.WriteLine("Density of rectangle ={0}", Density);
+                    Console.WriteLine("Second Moment of Inertia Ixx ={0}", IXX);
+                    Console.WriteLine("Second Moment of Inertia Iyy ={0}", IYY);
+                    Console.ReadKey();
+                }
+
+                catch (Exception e)
+                {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Error neustart erforderlich");
+                Console.ReadKey();
+
+                
+                
+
+                }
+            
+            
 
 
 
 
 
-            // Ausgaben
-          
-            Console.WriteLine("Perimeter of rectangle = {0}", Perimeter);
-            Console.WriteLine("Area of rectangle = {0}", Area);
-            Console.WriteLine("Volume of rectangle ={0}",Volume);
-            Console.WriteLine("Density of rectangle ={0}",Density);
-            Console.WriteLine("Second Moment of Inertia Ixx ={0}", IXX);
-            Console.WriteLine("Second Moment of Inertia Iyy ={0}",IYY);
-            Console.ReadKey();
 
+            
+           
 
 
 
