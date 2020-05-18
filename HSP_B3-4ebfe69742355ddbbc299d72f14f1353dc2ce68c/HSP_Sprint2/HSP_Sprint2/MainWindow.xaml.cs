7a49f35a10f7877a864c655f0095f375d52b0725
Haariger-Hugo <61110238+Haariger-Hugo@users.noreply.github.com>
2020-05-18 20:34:11 +0200

    Add files via upload

---
+++ HSP_B3-4ebfe69742355ddbbc299d72f14f1353dc2ce68c/HSP_Sprint2/HSP_Sprint2/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HSP_Sprint2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //InitializeComponent();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Rechteckprofil_selected(object sender, RoutedEventArgs e)
+        {
+            RechteckProfil_Grid.Visibility = Visibility.Visible;
+            Rundprofil_Grid.Visibility = Visibility.Hidden;
+            Rohrprofil_Grid.Visibility = Visibility.Hidden;
+            Rechteckrohrprofil_Grid.Visibility = Visibility.Hidden;
+            Tprofil_Grid.Visibility = Visibility.Hidden;
+            btn_brechnung_Rechteckprofil.Visibility = Visibility.Visible;
+            btn_brechnung_rundprofil.Visibility = Visibility.Hidden;
+            btn_berechnung_rohrprofil.Visibility = Visibility.Hidden;
+            btn_berechnung_rechteckrohrprofil.Visibility = Visibility.Hidden;
+            btn_brechnung_Tprofil.Visibility = Visibility.Hidden;
+
+
+
+        }
+
+        private void Rundprofil_selected(object sender, RoutedEventArgs e)
+        {
+            RechteckProfil_Grid.Visibility = Visibility.Hidden;
+            Rundprofil_Grid.Visibility = Visibility.Visible;
+            Rohrprofil_Grid.Visibility = Visibility.Hidden;
+            Rechteckrohrprofil_Grid.Visibility = Visibility.Hidden;
+            Tprofil_Grid.Visibility = Visibility.Hidden;
+            btn_brechnung_Rechteckprofil.Visibility = Visibility.Hidden;
+            btn_brechnung_rundprofil.Visibility = Visibility.Visible;
+            btn_berechnung_rohrprofil.Visibility = Visibility.Hidden;
+            btn_berechnung_rechteckrohrprofil.Visibility = Visibility.Hidden;
+            btn_brechnung_Tprofil.Visibility = Visibility.Hidden;
+        }
+
+        private void Rohrprofil_selected(object sender, RoutedEventArgs e)
+        {
+            RechteckProfil_Grid.Visibility = Visibility.Hidden;
+            Rundprofil_Grid.Visibility = Visibility.Hidden;
+            Rohrprofil_Grid.Visibility = Visibility.Visible;
+            Rechteckrohrprofil_Grid.Visibility = Visibility.Hidden;
+            Tprofil_Grid.Visibility = Visibility.Hidden;
+            btn_brechnung_Rechteckprofil.Visibility = Visibility.Hidden;
+            btn_brechnung_rundprofil.Visibility = Visibility.Hidden;
+            btn_berechnung_rohrprofil.Visibility = Visibility.Visible;
+            btn_berechnung_rechteckrohrprofil.Visibility = Visibility.Hidden;
+            btn_brechnung_Tprofil.Visibility = Visibility.Hidden;
+        }
+
+        private void Rechteckrohrprofil_selected(object sender, RoutedEventArgs e)
+        {
+            RechteckProfil_Grid.Visibility = Visibility.Hidden;
+            Rundprofil_Grid.Visibility = Visibility.Hidden;
+            Rohrprofil_Grid.Visibility = Visibility.Hidden;
+            Rechteckrohrprofil_Grid.Visibility = Visibility.Visible;
+            Tprofil_Grid.Visibility = Visibility.Hidden;
+            btn_brechnung_Rechteckprofil.Visibility = Visibility.Hidden;
+            btn_brechnung_rundprofil.Visibility = Visibility.Hidden;
+            btn_berechnung_rohrprofil.Visibility = Visibility.Hidden;
+            btn_berechnung_rechteckrohrprofil.Visibility = Visibility.Visible;
+            btn_brechnung_Tprofil.Visibility = Visibility.Hidden;
+        }
+
+        private void Tprofil_Selected(object sender, RoutedEventArgs e)
+        {
+            RechteckProfil_Grid.Visibility = Visibility.Hidden;
+            Rundprofil_Grid.Visibility = Visibility.Hidden;
+            Rohrprofil_Grid.Visibility = Visibility.Hidden;
+            Rechteckrohrprofil_Grid.Visibility = Visibility.Hidden;
+            Tprofil_Grid.Visibility = Visibility.Visible;
+            btn_brechnung_Rechteckprofil.Visibility = Visibility.Hidden;
+            btn_brechnung_rundprofil.Visibility = Visibility.Hidden;
+            btn_berechnung_rohrprofil.Visibility = Visibility.Hidden;
+            btn_berechnung_rechteckrohrprofil.Visibility = Visibility.Hidden;
+            btn_brechnung_Tprofil.Visibility = Visibility.Visible;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HSP_B3-4ebfe69742355ddbbc299d72f14f1353dc2ce68c/HSP_Sprint_1/HSP_Sprint_1/Program.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSP_Sprint_1
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            double m = 0;
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            bool checkrestart = false;
+
+            Console.WriteLine("Der Rechteckcalculator");
+            Console.WriteLine();
+
+           
+            do
+            {
+
+                a = Eingabe_a();                                //Länge a   
+                b = Eingabe_b();                                //Länge b 
+                c = Eingabe_c();                                //Lange c
+                m = Eingabe_m();                                // Material Auswahl
+                Ausgabe_Flaeche(a, b);
+                Ausgabe_Schwerpunkt(a, b, c);
+                Ausgabe_Flaechentragheitsmoment(a, b);
+                Volume_Ausgabe(a, b, c);
+                Ausgabe_Gewicht(a, b, c, m);
+
+                Console.WriteLine("Möchten Sie eine weitere Rechnung durchführen? y/n");
+                checkrestart = (Console.ReadKey().KeyChar == 'y');
+                Console.WriteLine();
+                Console.WriteLine("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+                Console.WriteLine();
+            } while (checkrestart);
+            Console.WriteLine("Das Programm wird beendet.");
+            Console.ReadKey();
+        }
+                                                                    // Eingaben a, b, c, M,
+        static double Eingabe_a()
+        {
+            double a = 0;
+            bool checkmate = true;
+
+            do
+            {
+                Console.WriteLine("Bitte geben Sie Länge a ein in cm.");
+                String eingabe_a = Console.ReadLine();
+
+
+                try
+                {
+                    a = Convert.ToDouble(eingabe_a);
+
+
+                     // Negative oder Null Werte werden abgefangen und Schleife beginnt erneut.
+                     
+                    if (a == 0)
+                    {
+                        checkmate = true;
+                        Console.WriteLine("Werte müssen größer als Null sein.");
+                    }
+                    else if(a <= 0)
+                    {
+                        checkmate = true;
+                        Console.WriteLine("Bitte keine negativen Werte eingeben.");
+                    }
+
+                    else if ( a >= 0)
+                    {
+                        checkmate = false;
+
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Der eingegebene Wert ist keine Zahl. Bitte geben sie eine Zahl ein.");
+                }
+
+
+            }
+            while (checkmate);
+            return a;
+        }
+
+        static double Eingabe_b()
+        {
+            double b = 0;
+            bool checkmate = true;
+
+            do
+            {
+                Console.WriteLine("Bitte geben Sie Länge b ein in cm.");
+                String eingabe_b = Console.ReadLine();
+                try
+                {
+                    b = Convert.ToDouble(eingabe_b);
+
+
+                    // Siehe Z. 64
+
+                    if (b == 0)
+                    {
+                        checkmate = true;
+                        Console.WriteLine("Werte müssen größer als Null sein.");
+                    }
+                    else if (b <= 0)
+                    {
+                        checkmate = true;
+                        Console.WriteLine("Bitte keine negativen Werte eingeben.");
+                    }
+                    else if (b >= 0)
+                    {
+                        checkmate = false;   
+                    
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Der eingegebene Wert ist keine Zahl. Bitte geben sie eine Zahl ein.");
+                }
+
+            }
+            while (checkmate);
+            return b;
+        }
+        
+        static double Eingabe_c()
+        {
+            double c = 0;
+            bool checkmate = true;
+
+            do
+            {
+                Console.WriteLine("Bitte geben Sie Länge c ein in cm.");
+                String eingabe_c = Console.ReadLine();
+
+                try
+                {
+                    c = Convert.ToDouble(eingabe_c);
+
+
+                    // Siehe Z. 64
+
+                    if (c == 0)
+                    {
+                        checkmate = true;
+                        Console.WriteLine("Werte müssen größer als Null sein.");
+                    }
+                    else if (c <= 0)
+                    {
+                        checkmate = true;
+                        Console.WriteLine("Bitte keine negativen Werte eingeben.");
+                    }
+                    else if (c >= 0)
+                    {
+                        checkmate = false;
+
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Der eingegebene Wert ist keine Zahl. Bitte geben sie eine Zahl ein.");
+                }
+
+            }
+            while (checkmate);
+            return c;
+            
+        }
+                                    //Auswahl Material, zwischen Stahl und Alu
+        static double Eingabe_m()               
+        {
+            double m = 0;
+            bool checkmate = true;
+
+            do
+            {
+                Console.WriteLine("Bitte wählen sie zwischen 1 für Stahl und 2 + für Aluminium.");
+                Console.WriteLine(" Dichte von Stahl beträgt: 7.85 g/cm^3 und Aluminium: 2.7g/cm^3.");
+                String eingabe_m = Console.ReadLine();
+
+                try
+                {
+                    m = Convert.ToDouble(eingabe_m);
+
+
+                    // Siehe Z. 64
+
+                    if (m == 1)
+                    {
+                        checkmate = false;
+                        Console.WriteLine("Stahl gewählt.");
+                        m = 7.85;
+
+                    }
+                    else if (m == 2)
+                    {
+                        checkmate = false;
+                        Console.WriteLine("Aluminium gewählt.");
+                        m = 2.7;
+
+                    }
+                    else
+                    {
+                        checkmate = true;
+                        Console.WriteLine("Falsche Eingabe, bitte zwischen 1 und 2 wählen.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Der eingegebene Wert ist keine Zahl. Bitte geben sie eine Zahl ein.");
+                }
+
+            }
+            while (checkmate);
+            return m;
+
+        }   
+
+            // Berechnungen
+
+
+
+            static double Flaeche_berechnen(double a, double b)
+            {
+                double flaeche = a * b;
+                return flaeche;
+            }
+            static double Volume_berechnen(double a, double b, double c)
+             {
+                double volume = a * b * c;
+                return volume;
+             }
+
+            static double Schwerpunkt_x_Rechteck_berechnen(double a)
+            {
+                double x = a / 2;
+                return x;
+            }
+        static double Schwerpunkt_y_Rechteck_berechnen(double b)
+        {
+            double y = b / 2;
+            return y;
+        }
+
+        static double Schwerpunkt_z_Rechteck_berechnen(double c)
+        {
+            double z = c / 2;
+            return z;
+
+        }
+
+        static double Flaechentraegheitsmoment_Rechteck_Iy_berechnen(double a, double b)
+        {
+            double Iy = (b * (Math.Pow(a, 3))) / 12;
+            return Iy;
+        }
+
+
+        static double Flaechentraegheitsmoment_Rechteck_Iz_berechnen(double a, double b)
+        {
+            double Iz = (a * (Math.Pow(b, 3))) / 12;
+            return Iz;
+        }
+
+        static double Gewicht_berechnen(double a, double b, double c, double m)
+        {
+            double Mass = a * b * c * m;
+            return Mass;
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+        // Ausgaben
+
+        static void Ausgabe_Gewicht(double a, double b, double c, double m)
+        {
+            Console.WriteLine("Gewicht ist:" + Gewicht_berechnen(a,b,c,m)+ " gramm");
+
+
+        }
+
+
+        static void Volume_Ausgabe(double a, double b, double c)
+        {
+            Console.WriteLine("Volume ist:" + Volume_berechnen(a, b, c) + " cm^3");
+
+        }
+
+        static void Ausgabe_Flaeche(double a, double b)
+        {
+            Console.WriteLine("Die Fläche beträgt: " + Flaeche_berechnen(a, b)+ "cm^2 ");
+          
+        }
+        static void Ausgabe_Schwerpunkt(double a, double b, double c)
+        {
+            Console.WriteLine("Der Schwerpunkt liegt bei:");
+            Console.WriteLine("x = " + Schwerpunkt_x_Rechteck_berechnen(a));
+            Console.WriteLine("y = " + Schwerpunkt_y_Rechteck_berechnen(b));
+            Console.WriteLine("z = " + Schwerpunkt_z_Rechteck_berechnen(c));
+        }
+
+        static void Ausgabe_Flaechentragheitsmoment(double a, double b)
+        {
+            Console.WriteLine("Das Flächenträgheitsmoment liegt bei:");
+            Console.WriteLine("Iy = " + Flaechentraegheitsmoment_Rechteck_Iy_berechnen(a, b) + "cm^4");
+            Console.WriteLine("Iz = " + Flaechentraegheitsmoment_Rechteck_Iz_berechnen(a, b) + "cm^4");
+            Console.WriteLine("Iyz = 0");
+        }
+
+            
+            
+        }
+    }
